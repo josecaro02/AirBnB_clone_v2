8da8888ffa33fad4e7a113d1878f742067af7719
Nicolas <nzparra@gmail.com>
2020-03-30 19:36:14 -0500

    [FIX]Style

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,9 @@
-/*A database hbnb_dev_db
-A new user hbnb_dev (in localhost)
-The password of hbnb_dev should be set to hbnb_dev_pwd
-hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
-hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
-If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail*/
+-- A database hbnb_dev_db
+-- A new user hbnb_dev (in localhost)
+-- The password of hbnb_dev should be set to hbnb_dev_pwd
+-- hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
+-- hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
+-- If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE DATABASE IF NOT EXISTS performance_schema;
 GRANT ALL ON hbnd_dev_db.*TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
