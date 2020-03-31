96fd69ea4466e02832958e358743b82277e5b860
Nicolas <nzparra@gmail.com>
2020-03-30 20:45:40 -0500

    [Fix]Password

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,6 @@
---A database hbnb_test_db
---A new user hbnb_test (in localhost)
---The password of hbnb_test should be set to hbnb_test_pwd
---hbnb_test should have all privileges on the database hbnb_test_db (and only this database)
---hbnb_test should have SELECT privilege on the database performance_schema (and only this database)
---If the database hbnb_test_db or the user hbnb_test already exists, your script should not fail
+-- Prepare the Mysql Server;
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE DATABASE IF NOT EXISTS performance_schema;
-GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
