7044acbfbf3ce66e5b5a5da609df02153a4c4111
Nicolas <nzparra@gmail.com>
2020-03-31 17:17:42 -0500

    [Update]chmod

--- models/city.py
+++ models/city.py
@@ -3,7 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
 
 
 class City(BaseModel, Base):
@@ -14,4 +13,4 @@ class City(BaseModel, Base):
     """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"))
