0d51f0a3cd08ce9363eabf71a870a1356fe01ea1
Jose David Caro <j.caro0207@gmail.com>
2020-03-31 22:08:06 -0500

    [Import environ in model place]

--- models/place.py
+++ models/place.py
@@ -6,6 +6,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from models.city import City
+from os import environ
 
 
 class Place(BaseModel, Base):
