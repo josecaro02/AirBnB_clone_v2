e6cbaf646e499bfffcd849bf7b93a3ce6a398e30
Jose David Caro <j.caro0207@gmail.com>
2020-04-02 17:01:47 -0500

    [Add SQL storage test]

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -2,6 +2,7 @@
 """test for state"""
 import unittest
 from os import environ
+from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -9,7 +10,6 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from models.engine.file_storage import FileStorage, DBStorage
 import MySQLdb
 import pep8
 
@@ -27,3 +27,18 @@ class TestDB(unittest.TestCase):
         style = pep8.StyleGuide(quiet=True)
         check = style.check_files(['models/engine/db_storage.py'])
         self.assertEqual(check.total_errors, 0, "View Pep8")
+
+    def test_create(self):
+        db = MySQLdb.connect(host=hots, user=DUser, passwd=pwd, db=dbase)
+        cur = db.cursor()
+        sql_1 = "SELECT COUNT(ID) FROM states"
+        cur.execute(sql_1)
+        result_1 = cur.fetchall()[0][0]
+        state = State(name="Joseland")
+        state.save()
+        storage.save()
+        db.commit();
+        sql_2 = "SELECT COUNT(*) FROM states"
+        cur.execute(sql_2)
+        result_2 = cur.fetchall()[0][0]
+        self.assertEqual(result_1 + 1, result_2)
