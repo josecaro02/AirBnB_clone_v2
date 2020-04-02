394ae058039fb989a2d56a095985c52b8853d77d
Nicolas <nzparra@gmail.com>
2020-04-02 16:49:06 -0500

    [ADD]Pep8 Db

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -2,8 +2,14 @@
 """test for state"""
 import unittest
 from os import environ
-from models import *
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.file_storage import FileStorage, DBStorage
 import MySQLdb
 import pep8
 
@@ -15,21 +21,6 @@ dbase = environ.get('HBNB_MYSQL_DB')
 
 class TestDB(unittest.TestCase):
     """ Test State Class in DB storage """
-
-    def test_create(self):
-        db = MySQLdb.connect(host=hots, user=DUser, passwd=pwd, db=dbase)
-        cur = db.cursor()
-        sql_1 = "SELECT COUNT(*) FROM states"
-        cur.execute(sql_1)
-        result_1 = cur.fetchall()[0][0]
-        state = State(name="Joseland")
-        state.save()
-        storage.save()
-        db.commit()
-        sql_2 = "SELECT COUNT(*) FROM states"
-        cur.execute(sql_2)
-        result_2 = cur.fetchall()[0][0]
-        self.assertEqual(result_1 + 1, result_2)
 
     def test_pep8_DB(self):
         """Test pep8 """
