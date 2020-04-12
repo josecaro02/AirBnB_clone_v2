6ba8a221fe9b7441c65d3b6f1e123605bb501a07
Jose David Caro <j.caro0207@gmail.com>
2020-04-11 22:47:40 -0500

    [Update task 0]

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
 #Install Nginx
-sudo apt-get install nginx
+sudo apt-get update
+sudo apt-get install -y nginx
 #Create folder /data/web_static/releases
 sudo mkdir -p /data/web_static/releases/test
 #Create folder /data/web/shared
 sudo mkdir -p /data/web_static/shared
-#Create fake HTML file
-fake_html="<html>
-	<head>\n\t</head>
-	<body>\n\t\tHolberton School\n\t</body>
-</html>
-"
-echo "$fake_html" > /data/web_static/releases/test/index.html
-# Create Symbolic link
-
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 #Give permissions to /data/folder
-sudo chown -R ubuntu:ubuntu data/
+sudo chown -R ubuntu:ubuntu /data/
+
+#Create fake HTML file
+fake_html="<html>
+	<head>
+        </head>
+	<body>Holberton School
+        </body>
+</html>
+"
+sudo echo "$fake_html" > /data/web_static/releases/test/index.html
+# Create Symbolic link
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 #Update NGINX configuration to serve /data/web_static/current/ to hbnb_static
-base_txt="server_name _;"
-new_txt="server_name _;\n\tlocation \/hbnb_static {\n\talias  \/data\/web_static\/current;\n\t}"
-sudo sed -i "s/$base_text/$new_txt/" /etc/nginx/sites-available/default
+new_txt="server_name _;\n\tlocation \/hbnb_static\/ {\n\talias \/data\/web_static\/current\/;\n\tautoindex off;\n\t}\n"
+sudo sed -i "s/server_name _;/$new_txt/" /etc/nginx/sites-available/default
+
+#Restart nginx
+sudo service nginx restart
