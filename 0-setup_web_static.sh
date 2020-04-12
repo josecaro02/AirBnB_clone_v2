a6542e455df32cf82b236b26c0cbb45c620f6c63
Jose David Caro <j.caro0207@gmail.com>
2020-04-11 21:43:13 -0500

    [Add task 0]

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#Install Nginx
+sudo apt-get install nginx
+#Create folder /data/web_static/releases
+sudo mkdir -p /data/web_static/releases/test
+#Create folder /data/web/shared
+sudo mkdir -p /data/web_static/shared
+#Create fake HTML file
+fake_html="<html>
+	<head>\n\t</head>
+	<body>\n\t\tHolberton School\n\t</body>
+</html>
+"
+echo "$fake_html" > /data/web_static/releases/test/index.html
+# Create Symbolic link
+
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
+#Give permissions to /data/folder
+sudo chown -R ubuntu:ubuntu data/
+
+#Update NGINX configuration to serve /data/web_static/current/ to hbnb_static
+base_txt="root \/usr\/share\/nginx\/html"
+new_txt="root \/data\/web_static\/current"
+sudo sed -i "s/$base_text/new_txt/" /etc/nginx/sites-available/default
