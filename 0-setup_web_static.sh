bc64aab4d8a808ed72b6bd7887fd71defe610eba
Jose David Caro <j.caro0207@gmail.com>
2020-04-12 17:05:40 -0500

    [Remove add auto index]

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,6 @@ mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-new_txt="server_name _;\n\tlocation \/hbnb_static\/ {\n\talias \/data\/web_static\/current\/;\n\tautoindex off;\n\t}\n"
+new_txt="server_name _;\n\tlocation \/hbnb_static\/ {\n\talias \/data\/web_static\/current\/;\n\t}\n"
 sed -i "s/server_name _;/$new_txt/" /etc/nginx/sites-available/default
 service nginx restart
