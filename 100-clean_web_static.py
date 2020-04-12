1dea1c52e8e14c90866a45bd81fb71d7c8830ce8
Jose David Caro <j.caro0207@gmail.com>
2020-04-12 15:31:56 -0500

    [Update task 100 with recursive and force remove]

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -60,6 +60,6 @@ def do_clean(number=0):
         number == 2
     else:
         number +=1
-    local("cd versions ; ls -t | tail -n +{} | xargs rm ".format(number))
+    local("cd versions ; ls -t | tail -n +{} | xargs rm -rf ".format(number))
     location="/data/web_static/releases"
-    run("cd {} ; ls -t | tail -n +{} | xargs rm ".format(location, number))
+    run("cd {} ; ls -t | tail -n +{} | xargs rm -rf".format(location, number))
