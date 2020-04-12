6424ae251ee81cad54a1a7e5eb6e9839e30fd54a
Jose David Caro <j.caro0207@gmail.com>
2020-04-12 01:07:16 -0500

    [change text in echo]

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,16 +4,9 @@ apt-get update
 apt-get install -y nginx
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
-fake_html="<html>
-	<head>
-        </head>
-	<body>Holberton School
-        </body>
-</html>"
-echo "$fake_html" > /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 new_txt="server_name _;\n\tlocation \/hbnb_static\/ {\n\talias \/data\/web_static\/current\/;\n\tautoindex off;\n\t}\n"
 sed -i "s/server_name _;/$new_txt/" /etc/nginx/sites-available/default
 service nginx restart
-exit 0
