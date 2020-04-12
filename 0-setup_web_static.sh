ff55c93cd510e8cfda02ece5028c256fbe209a1d
Jose David Caro <j.caro0207@gmail.com>
2020-04-11 22:55:38 -0500

    [Remove comments in task 0]

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,30 +2,17 @@
 #Install Nginx
 apt-get update
 apt-get install -y nginx
-#Create folder /data/web_static/releases
 mkdir -p /data/web_static/releases/test
-#Create folder /data/web/shared
 mkdir -p /data/web_static/shared
-
-#Give permissions to /data/folder
-chown -R ubuntu:ubuntu /data/
-
-#Create fake HTML file
 fake_html="<html>
 	<head>
         </head>
 	<body>Holberton School
         </body>
-</html>
-"
+</html>"
 echo "$fake_html" > /data/web_static/releases/test/index.html
-# Create Symbolic link
-
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-#Update NGINX configuration to serve /data/web_static/current/ to hbnb_static
+chown -R ubuntu:ubuntu /data/
 new_txt="server_name _;\n\tlocation \/hbnb_static\/ {\n\talias \/data\/web_static\/current\/;\n\tautoindex off;\n\t}\n"
 sed -i "s/server_name _;/$new_txt/" /etc/nginx/sites-available/default
-
-#Restart nginx
 service nginx restart
