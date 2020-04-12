e598f83f273c96eb39f4445c2b22185c0bcaf49e
Jose David Caro <j.caro0207@gmail.com>
2020-04-11 22:50:19 -0500

    [Remove sudo to commands]

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 #Install Nginx
-sudo apt-get update
-sudo apt-get install -y nginx
+apt-get update
+apt-get install -y nginx
 #Create folder /data/web_static/releases
-sudo mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/releases/test
 #Create folder /data/web/shared
-sudo mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/shared
 
 #Give permissions to /data/folder
-sudo chown -R ubuntu:ubuntu /data/
+chown -R ubuntu:ubuntu /data/
 
 #Create fake HTML file
 fake_html="<html>
@@ -18,14 +18,14 @@ fake_html="<html>
         </body>
 </html>
 "
-sudo echo "$fake_html" > /data/web_static/releases/test/index.html
+echo "$fake_html" > /data/web_static/releases/test/index.html
 # Create Symbolic link
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 #Update NGINX configuration to serve /data/web_static/current/ to hbnb_static
 new_txt="server_name _;\n\tlocation \/hbnb_static\/ {\n\talias \/data\/web_static\/current\/;\n\tautoindex off;\n\t}\n"
-sudo sed -i "s/server_name _;/$new_txt/" /etc/nginx/sites-available/default
+sed -i "s/server_name _;/$new_txt/" /etc/nginx/sites-available/default
 
 #Restart nginx
-sudo service nginx restart
+service nginx restart
