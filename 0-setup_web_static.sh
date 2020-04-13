869dccf543c9b342842107cf04ea2f71a32806d6
Jose David Caro <j.caro0207@gmail.com>
2020-04-12 22:13:39 -0500

    [add task 0]

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,8 @@ apt-get install -y nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 new_txt="server_name _;\n\tlocation \/hbnb_static\/ {\n\talias \/data\/web_static\/current\/;\n\t}\n"
 sed -i "s/server_name _;/$new_txt/" /etc/nginx/sites-available/default
 service nginx restart
